4b15efbb53955462c2a956ac2bbc635b55bf28cb
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2024-12-16 13:21:36 -0300

    alteracoes esteticas na home

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Home from './components/Home/Home';
-import Cadastro from './components/Cadastro/Cadastro';
-import Login from './components/Login/Login';
 
 function App() {
     document.title = 'To-Do List';
     return (
         <div className="App">
-            <Home />
+            <Home/>
         </div>
     );
 }

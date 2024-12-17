c2cf4710e22b88bdae4dd4cb11e339f52b1eddcd
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2024-12-16 22:57:02 -0300

    alteracao estetica leve em tarefa

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+
 import Home from './pages/Home/Home';
 import Cadastro from './pages/Cadastro/Cadastro';
 import Login from './pages/Login/Login';
 
+import { createBrowserRouter, createRoutesFromElements,
+        Route, RouterProvider } from 'react-router';
+
+        
+const browserRouter = createBrowserRouter(createRoutesFromElements(
+    <Route path='/'>
+        <Route index element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='cadastro' element={<Cadastro />} />
+    </Route>
+))
 
 function App() {
     document.title = 'To-Do List';
     return (
-        <div className="App">
-            <Home/>
-        </div>
+        <RouterProvider router={browserRouter}/>
     );
 }
 
